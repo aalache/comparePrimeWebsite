bd19f64351a02d7736c33b7a3883a0aa6b2949a5
aalache <oaalache@gmail.com>
2023-08-15 10:02:01 -0400

    test3

--- client/src/components/Form.tsx
+++ client/src/components/Form.tsx
@@ -42,7 +42,9 @@ export default function Form(){
 
         fetch("http://localhost:3000/send", {
             method: "POST",
-            headers: { 'Content-type': 'application/json; charset=UTF-8' },
+            headers: { 'Content-type': 'application/json; charset=UTF-8',
+                        'Access-Control-Allow-Origin': "http://localhost:5173",
+                     },
             body:JSON.stringify({
                 nom:data.nom,
                 prenom:data.prenom,
