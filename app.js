bf5ebe42b91d5c5893ccaaff1278c77f5cd0b7b1
aalache <oaalache@gmail.com>
2023-08-15 09:31:04 -0400

    test2

--- app.js
+++ app.js
@@ -17,15 +17,15 @@ const port = process.env.SERVER_PORT ;
 
 app.use(
     cors({
-        origin: '*',
+        origin: 'http://localhost:5173',
     })
 )
 
-app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', ['*']);
-    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.append('Access-Control-Allow-Origin', ['*']);
+//     res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//     next();
+// });
 
 app.use(express.json());
 app.use(express.urlencoded({ 
