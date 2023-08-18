9d3ea29a360b435c8acd74e2b25d95f141bb4488
aalache <oaalache@gmail.com>
2023-08-18 14:30:40 -0400

    get req

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ app.use((req, res, next) => {
     res.append('Access-Control-Allow-Origin', ['*']);
     res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.append('Access-Control-Allow-Headers', 'Content-Type');
-    res.send(200);
     next();
 });
 
