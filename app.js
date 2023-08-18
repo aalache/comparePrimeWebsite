99a891ef655a8760fa211f66c58df26d3183e699
aalache <oaalache@gmail.com>
2023-08-18 11:13:09 -0400

    setting headers

--- app.js
+++ app.js
@@ -12,10 +12,16 @@ const app = express();
 const port = process.env.SERVER_PORT ;
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     // methods:["GET","POST"]
 }))
 
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 
 app.use(express.json());
