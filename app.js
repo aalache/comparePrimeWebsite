5e6e02de74485e1613c228b7eaed0e9bca94a912
aalache <oaalache@gmail.com>
2023-08-18 11:07:44 -0400

    origin set to port 5173

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 const port = process.env.SERVER_PORT ;
 
 app.use(cors({
-    // origin: "http://localhost:5173",
+    origin: "http://localhost:5173",
     // methods:["GET","POST"]
 }))
 
